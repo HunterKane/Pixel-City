b82bfffb8f156a2b32fe3bfcb17888ceb23000e3
Hunter Sparrow <HunterSparrow@hunters-macbook-pro.local>
2018-06-09 16:21:35 +0900

    Initial Commit

---
+++ Pixel City/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pixel City
+//
+//  Created by Hunter Sparrow on 6/9/18.
+//  Copyright © 2018 Hunter Sparrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
